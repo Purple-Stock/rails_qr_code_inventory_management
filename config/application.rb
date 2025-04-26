7ba522c36a80ea7fae12836d84ce129db4679fd6
puppe1990 <matheus.puppe90@hotmail.com>
2025-04-26 19:18:38 -0300

    Add internationalization support: integrate 'rails-i18n' gem, implement locale switching in the application, and add English and Brazilian Portuguese translations for sidebar elements and other UI text.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module RailsQrCodeInvetoryManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Available locales
+    config.i18n.available_locales = [:en, :"pt-BR"]
+    
+    # Default locale
+    config.i18n.default_locale = :en
+    
+    # Load locale files from config/locales/*.{rb,yml}
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
