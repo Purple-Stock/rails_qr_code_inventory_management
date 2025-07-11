9b783206f1534130b2a865099ed1256163c270e6
Matheus Nunes Puppe <matheus.puppe90@hotmail.com>
2025-07-11 18:13:46 -0300

    Stop double click error

--- app/javascript/controllers/password_visibility_controller.js
+++ app/javascript/controllers/password_visibility_controller.js
@@ -3,9 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input", "eye", "eyeSlash"]
 
-  toggle() {
+  toggle(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
     const type = this.inputTarget.type === "password" ? "text" : "password"
     this.inputTarget.type = type
+
     if (this.hasEyeTarget && this.hasEyeSlashTarget) {
       this.eyeTarget.classList.toggle("hidden", type === "text")
       this.eyeSlashTarget.classList.toggle("hidden", type === "password")
