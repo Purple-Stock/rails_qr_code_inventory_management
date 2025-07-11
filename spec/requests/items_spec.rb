005ab49b3ba164dfc23444e66c59cccfe01df79b
Matheus Nunes Puppe <matheus.puppe90@hotmail.com>
2025-07-11 20:29:35 -0300

    Add CSV import for items

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -31,4 +31,38 @@ RSpec.describe "Items", type: :request do
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
+
+  describe "GET /teams/:team_id/items/import" do
+    it "shows the import form" do
+      get import_team_items_path(team)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /teams/:team_id/items/import" do
+    let(:csv_content) do
+      "Name,SKU,Barcode,Type,Current Stock,Price,Cost,Brand\n" \
+        "CSV Item,CSV1,BC1,Type,1,2,1,Brand"
+    end
+
+    it "imports items" do
+      file = Tempfile.new(%w[items csv])
+      file.write(csv_content)
+      file.rewind
+
+      expect {
+        post import_team_items_path(team), params: { file: fixture_file_upload(file.path, 'text/csv') }
+      }.to change(Item, :count).by(1)
+
+      expect(response).to redirect_to(team_items_path(team))
+    ensure
+      file.close
+      file.unlink
+    end
+
+    it "rejects request without file" do
+      post import_team_items_path(team)
+      expect(response).to redirect_to(import_team_items_path(team))
+    end
+  end
 end
