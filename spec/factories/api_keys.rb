114e5656054fc5c645ee0ff8d09825c95423ed03
puppe1990 <matheus.puppe90@hotmail.com>
2025-07-12 08:31:58 -0300

    Enhance API key model with schema information and update database schema to include api_keys table with foreign key constraints

--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: api_keys
+#
+#  id         :bigint           not null, primary key
+#  token      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_api_keys_on_token    (token) UNIQUE
+#  index_api_keys_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :api_key do
     association :user
