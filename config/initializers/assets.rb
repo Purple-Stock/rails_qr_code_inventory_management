09e6f2abc88859c922c12d13b5c39cfd2ac72162
Matheus Nunes Puppe <matheus.puppe90@hotmail.com>
2025-07-30 14:53:17 -0300

    Add stylesheet components path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Include subdirectories for component-based stylesheets
+Rails.application.config.assets.paths << Rails.root.join("app/assets/stylesheets/components")
